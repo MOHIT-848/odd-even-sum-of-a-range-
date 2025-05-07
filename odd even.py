40c5c1ad6cb9b4df20978e04224949b83e365c14
MOHIT-848 <singhsaabh1548@gmail.com>
2025-05-07 15:55:33 +0530

    Add files via upload

---
+++ odd even.py
@@ -0,0 +1,6 @@
+# code to check number is odd or even
+a= int(input("enter the number of your choice ="))
+if a%2==0:
+    print("its a even number")
+else :
+    print("its a odd number ")
---
+++ sum.py
@@ -0,0 +1,5 @@
+# sum of a range of numbers
+ans= 0
+for a in range (1,51):
+    ans= ans+a
+print("the sum is ", ans)
